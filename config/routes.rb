78e28917427bc1b27df1f755c1958736187f24d2
Von Ashley & Louie Chen <pair+Von-Ashley.louiehchen@devbootcamp.com>
2016-12-13 15:52:56 -0800

    skeleton add user auth

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  resources :users
+  root to: "users#index"
+
+  resource :sessions, only: [:new, :create, :destroy]
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  # delete '/logout', to: 'sessions#destroy', as: 'logout'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
